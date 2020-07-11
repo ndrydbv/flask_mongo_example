1cc6ff3b666386c170ae2bc473c232d9d9b23bb9
andrey <ndrydbv@gmail.com>
2020-07-11 19:53:24 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://<username>:<password>@cluster0-yzvvw.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    return '''
+        <form method="POST" action="/create" enctype="multipart/form-data">
+            <input type="text" name="username">
+            <input type="file" name="profile_image">
+            <input type="submit">
+        </form>
+    '''
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    if 'profile_image' in request.files:
+        profile_image = request.files['profile_image']
+        mongo.save_file(profile_image.filename, profile_image)
+        mongo.db.users.insert({'username': request.form.get('username'),
+                               'profile_image_name': profile_image.filename})
+        return 'Done!'
+
+
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = mongo.db.users.find_one_or_404({'username': username})
+    return f"""
+        <h1>{username}</h1>
+        <img src="{url_for('file', filename=user['profile_image_name'])}">
+    """
